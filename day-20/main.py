4eeb075e54769fec2e9715221ebbae842ab0b314
Mahdiyeh Ayooman <92025109+MahdiyehAyuoman@users.noreply.github.com>
2025-08-19 19:48:08 +0330

    Update main.py

--- day-20/main.py
+++ day-20/main.py
@@ -27,47 +27,6 @@ while game_is_on:
 
 
 
-# ## Frist try to make a snake
-# # s_1 = Turtle()
-# # s_1.color("white")
-# # s_1.shape("square")
-
-# # s_2 = Turtle()
-# # s_2.color("white")
-# # s_2.shape("square")
-# # s_2.goto(-20,0)
-
-# # s_3 = Turtle()
-# # s_3.color('white')
-# # s_3.shape("square")
-# # s_3.goto(-40,0)
-
-## define a function for make the snake 
-# x_y_posiotion = [(0,0), (-20,0), (-40,0)]
-# segment = []
-# def make_snake(x_y_posiotion):
-#     for posotion in x_y_posiotion:
-#         snake = Turtle()
-#         snake.penup()
-#         snake.color("white")
-#         snake.shape("square")
-#         snake.goto(posotion)
-#         segment.append(snake)
-
-# make_snake(x_y_posiotion)
-
-
-## Move Snake
-# game_is_on = True
-# while game_is_on:
-#     screen.update()
-#     time.sleep(0.1)
-#     for seg_num in range(len(segment)-1, 0, -1):
-#         new_x = segment[seg_num - 1].xcor()
-#         new_y = segment[seg_num - 1].ycor()
-#         segment[seg_num].goto(new_x, new_y)
-#     segment[0].forward(20)
-#     # segment[0].right(90)
 
 
 screen.exitonclick()
