b8e6be59ceadadfe0fbbec158cbb5ac078f217dd
Mahdiyeh Ayooman <mahdiyehayuoman@gmail.com>
2025-09-30 18:50:03 +0330

    Day 26 - List Comprehension and the NATO Alphabet

--- day-26/main.py
+++ day-26/main.py
@@ -1,15 +1,18 @@
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
+import pandas as pd
 
-import pandas
-
-data = pandas.read_csv("nato_phonetic_alphabet.csv")
 #TODO 1. Create a dictionary in this format:
-phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
-print(phonetic_dict)
+{"A": "Alfa", "B": "Bravo"}
+
+nato_phenonetic_data = pd.read_csv("nato_phonetic_alphabet.csv")
+nato_phenonetic_dic = {row.letter: row.code for (index, row) in nato_phenonetic_data.iterrows()}
+# print(nato_phenonetic_dic)
+
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+user_word = input('Enter a word: ')
+user_word_letters = [letter.upper() for letter in user_word]
+user_word_phonetic_list = [word for i in user_word_letters for (letter, word) in nato_phenonetic_dic.items() if letter == i]
 
-word = input("Enter a word: ").upper()
-output_list = [phonetic_dict[letter] for letter in word]
-print(output_list)
+print(user_word)
+print(user_word_letters)
+print(user_word_phonetic_list)
